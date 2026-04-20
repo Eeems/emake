24c3c251701dc0a2411c3cba48b90b6839ac2f2b
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2026-04-20 12:09:15 -0600

    Remove hardcoded version tests

--- tests/test_emake.py
+++ tests/test_emake.py
@@ -1,20 +1,12 @@
 """Tests for emake module."""
 
 
-def test_version():
-    """Test that version is set correctly."""
-    from emake import __version__
-
-    assert __version__ == "0.0.1"
-
-
 def test_config():
     """Test that config can be loaded."""
     from emake.config import get_project_config
 
     config = get_project_config()
     assert config.name == "emake"
-    assert config.version == "0.0.1"
 
 
 def test_venv():
